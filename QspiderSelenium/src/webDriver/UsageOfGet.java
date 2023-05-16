52838c8eae07f2feb9647d9988b6d4dc1c3578b5
inoob <inoob@192.168.244.1>
2023-05-17 00:08:33 +0530

    17/05/23

--- QspiderSelenium/src/webDriver/UsageOfGet.java
+++ QspiderSelenium/src/webDriver/UsageOfGet.java
@@ -2,7 +2,6 @@ package webDriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 /*
  * void get(String URL) ==>
